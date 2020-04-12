f361d90a9f53e31ad60371cfb0a66c08d02bc586
Nikola Kolevski 151221 <nikolamajkati@gmail.com>
2020-04-12 20:31:04 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -3,4 +3,16 @@ class Student {
 	String ime;
 	String prezime;
 ArrayList<Integer> ocenki= new ArrayList<Integer>();
+
+ double getAverage( ocenki ) {
+suma = 0;
+for( i = 0; i < ocenki.legnth ; i ++)
+	{
+	  suma = suma + ocenka[i]  
+	}
+	return suma
+	
+}
+ int ECTSCredits()
+	return ocenki.legnth * 6
 }
